67d072bcc626d334edc583b2a5b596685a86cb26
Justin Rowles <justin.rowles@bgch.co.uk>
2015-03-17 09:46:53 +0000

    Refactoring locations and adding a basic encrypt by name/team

---
+++ encrypt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd $(dirname $0)
+install_location=$(pwd)
+echo $install_location
+cd - &>/dev/null 
+
+inputfile=$1; shift
+recipients=$1; shift
+outputfile=$1; shift
+
+if [[ $1 ]]; then
+   echo "Too many arguments"
+fi
+
+if [[ -z $recipients ]]; then
+   recipients=all
+fi
+
+if [[ -z $outputfile ]]; then
+   outputfile=$inputfile.gpg
+fi
+
+if [[ -f $install_location/teams/$recipients ]]; then
+   recipients=$(cat $install_location/teams/$recipients | sed 's/^/-r /' | xargs)
+else
+   recipients="-r $recipients"
+fi
+
+gpg --encrypt $recipients $inputfile > $outputfile 
